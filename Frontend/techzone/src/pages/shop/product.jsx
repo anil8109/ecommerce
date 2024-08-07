630de6e26b365fbb85f7aa16d1cdab7b528eaf45
Anil Kumar Sen <anil810966@gmail.com>
2024-08-07 22:59:34 +0530

    adding front-end initial commit

---
+++ Frontend/techzone/src/pages/shop/product.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+
+export const Product = (props) => {
+  const { id, productName, price, productImage } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemCount = cartItems[id];
+
+  return (
+    <div className="product">
+      <img src={"https://nagarro-aws-training.s3.us-east-2.amazonaws.com/3.png"} />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p> ${price}</p>
+      </div>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
+      </button>
+    </div>
+  );
+};
